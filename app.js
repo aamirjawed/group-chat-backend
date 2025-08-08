d98820e414c8cf0cd1f77aaf439f1def609eb0f4
aamirjawed <aamirjawed682@gmail.com>
2025-08-08 07:32:24 +0530

    changes

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ process.on('unhandledRejection', (reason, promise) => {
 
 // CORS configuration
 app.use(cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
